0344f28bbc07fef078c494d792f377e78e3c2f56
tileOtter <adamfoster913@gmail.com>
2026-02-18 00:20:36 -0500

    Added block_to_block_type function to block.py and tests.

--- src/block.py
+++ src/block.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     stripped_blocks = []
@@ -6,3 +16,19 @@ def markdown_to_blocks(markdown):
         if b:
             stripped_blocks.append(b)
     return stripped_blocks
+
+def block_to_block_type(md_block):
+    lines = md_block.split("\n")
+    prefix = lines[0].split(" ")[0]
+    if all(c == "#" for c in prefix) and len(prefix) >= 1 and len(prefix) <= 6 and lines[0].startswith(prefix + " "):
+        return BlockType.HEADING
+    if lines[0] == "```" and lines[-1] == "```" and len(lines) > 2:
+        return BlockType.CODE
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    if all(line.startswith(f"{i}. ") for i, line in enumerate(lines, start=1)):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,5 +1,5 @@
 import unittest
-from block import markdown_to_blocks
+from block import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -21,6 +21,31 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_type_heading(self):
+        heading = "# Heading"
+        type = block_to_block_type(heading)
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_type_code(self):
+        code = "```\n code block \n```"
+        type = block_to_block_type(code)
+        self.assertEqual(type, BlockType.CODE)
+
+    def test_block_type_quote(self):
+        quote = "> Quote\n> lines\n> here"
+        type = block_to_block_type(quote)
+        self.assertEqual(type, BlockType.QUOTE)
+
+    def test_block_type_unordered_list(self):
+        list = "- Item\n- Another Item\n- A third item"
+        type = block_to_block_type(list)
+        self.assertEqual(type, BlockType.UNORDERED_LIST)
+
+    def test_block_type_ordered_list(self):
+        list = "1. One\n2. Two\n3. Three"
+        type = block_to_block_type(list)
+        self.assertEqual(type, BlockType.ORDERED_LIST)
 
 if __name__ == "__main__":
     unittest.main()
