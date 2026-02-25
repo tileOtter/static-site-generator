71b627ac9f2ce0463da5e873b7ba8f9fa8b39317
tileOtter <adamfoster913@gmail.com>
2026-02-24 23:12:37 -0500

    Added copy_files.py to copy files from the static directory to the public directory, and updated main to utilize this function.

--- src/copy_files.py
+++ src/copy_files.py
@@ -1,1 +1,16 @@
 # Recursive function, first deletes contents of public/ directory, then copies the contents of the static directory to the public directory
+import shutil
+import os
+
+def static_directory_copy(destination, source):
+    os.mkdir(destination)
+    source_contents = os.listdir(source)
+    for item in source_contents:
+        path = os.path.join(source, item)
+        extended_path = os.path.join(destination, item)
+        if os.path.isfile(path):
+            shutil.copy(path, extended_path, follow_symlinks=True)
+            print(f'Copied file {item} to: {extended_path}')
+        else:
+            static_directory_copy(extended_path, path)
+    
--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from textnode import TextNode, TextType
+from copy_files import static_directory_copy
+import os
+import shutil
 
 def main():
-    pass
-    # run copy_files
-    # generate public directory and all of the copied content properly 
+    destination = './public/'
+    source = './static/'
+    if os.path.exists(destination):
+        try:
+            shutil.rmtree(destination)
+            print(f'Removed {destination}.')
+        except Exception as e:
+            print(f'Error: {e}')
+    else:
+        print(f'Directory {destination} not found.')
+        
+    static_directory_copy(destination, source)
 
 if __name__ == "__main__":
     main()
